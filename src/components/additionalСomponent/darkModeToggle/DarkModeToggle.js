bcb71fe3f339f0721a802762622551b163b90d66
Alex Lemberg <alexlemberg90@gmail.com>
2022-10-28 21:31:35 +0300

    12

--- src/components/additionalСomponent/darkModeToggle/DarkModeToggle.js
+++ src/components/additionalСomponent/darkModeToggle/DarkModeToggle.js
@@ -1,24 +1,25 @@
-import "./toggle.module.css";
-import {createContext, useState} from "react";
-import ReactSwitch from "react-switch";
-
-
-export const ThemeContext = createContext(null);
+import React, { useState } from 'react';
+import"./toggle.module.css"
+import Movie from "../../movies/Movie";
 
 export default function DarkModeToggle() {
-    const [theme, setTheme] = useState("dark");
 
-    const toggleTheme = () => {
-        setTheme((curr) => (curr === "light" ? "dark" : "light"));
-    };
+    const [isClicked, setIsClicked] = useState(false);
+
+    function darkMode() {
+        setIsClicked(isClicked => !isClicked);
+    }
+
+    let color1 = "orange"
+    let color2 = "blue"
+
     return (
-        <ThemeContext.Provider value={{theme, toggleTheme}}>
-            <div className="App" id={theme}>
-                <div className="switch">
-                    <label> {theme === "light" ? "Light Mode" : "Dark Mode"}</label>
-                    <ReactSwitch onChange={toggleTheme} checked={theme === "dark"}/>
-                </div>
-            </div>
-        </ThemeContext.Provider>
+
+        <div className="App" style={isClicked ? ( {backgroundColor: `#181818`}) : (
+            {backgroundImage: `linear-gradient(135deg, ${color1}, ${color2})`}
+        )}>
+
+            <button onClick={darkMode}> Click for Dark Mode </button>
+        </div>
     );
 }
